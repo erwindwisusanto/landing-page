2fd682a9299436d602a4c6d33417428eff3481ef
erwin dwi s <erwin@waki.mobi>
2024-02-26 14:34:24 +0700

    ketinggalan

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,79 +1,40 @@
 <footer class="footer">
-  <section class="bg-footer py-4 py-md-5 py-xl-8">
-    <div class="container overflow-hidden px-4">
-      <div class="row gy-4 gy-lg-0 justify-content-xl-between">
-        <div class="col-6 col-md-4 col-lg-3 col-xl-2 d-flex align-items-center">
-          <div class="widget">
-            <a href="#!">
-              <img src="{{ asset('assets/img/logo-white-cepat-sehat.svg') }}" alt="cepat sehat">
-            </a>
-          </div>
-        </div>
-				<div class="col-6 col-md-4 col-lg-3 col-xl-2 d-flex justify-conten-end align-items-center">
-          <div class="widget" style="text-align: right;">
-						<div class="row">
-							<span class="txt-white" style="font-size: 13px;">
-								Member of Aliansi Telemedik Indonesia
-							</span>
-						</div>
-						<div class="row">
-							<a clas href="https://atensi.or.id/">
-								<img src="{{ asset('assets/img/logo-atensi.svg') }}" alt="cepat sehat">
-						</div>
-            </a>
-          </div>
-        </div>
-        <div class="col-12 col-md-4 col-lg-3 col-xl-2 pt-4">
-          <div class="widget">
-            <h4 class="widget-title mb-4 txt-white" style="font-weight: 500;">PT Cepat Sehat Indonesia</h4>
-            <address class="mb-4 txt-white" style="font-size: 13px;">Jalan Peternakan No. 13, Kel. Tengah, Kec. Kramat jati, Kota Adm. Jakarta Timur, Provinsi DKI Jakarta 13510
-							sehatcepat.com</address>
-            <p class="mb-1">
-              <a class="txt-white text-decoration-none" href="tel:+62822 1118 9009" style="font-weight: 500;">
-								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
-									<path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
-								</svg>
-								0822 1118 9009</a>
-            </p>
-          </div>
-        </div>
-				<div class="col-xs-12 col-md-5 order-0 order-md-1">
-          <ul class="nav justify-content-center justify-content-md-end">
-            <li class="nav-item">
-              <a class="nav-link txt-white" href="#!">
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-globe" viewBox="0 0 16 16">
-									<path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855A8 8 0 0 0 5.145 4H7.5zM4.09 4a9.3 9.3 0 0 1 .64-1.539 7 7 0 0 1 .597-.933A7.03 7.03 0 0 0 2.255 4zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a7 7 0 0 0-.656 2.5zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5zM8.5 5v2.5h2.99a12.5 12.5 0 0 0-.337-2.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5zM5.145 12q.208.58.468 1.068c.552 1.035 1.218 1.65 1.887 1.855V12zm.182 2.472a7 7 0 0 1-.597-.933A9.3 9.3 0 0 1 4.09 12H2.255a7 7 0 0 0 3.072 2.472M3.82 11a13.7 13.7 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm6.853 3.472A7 7 0 0 0 13.745 12H11.91a9.3 9.3 0 0 1-.64 1.539 7 7 0 0 1-.597.933M8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855q.26-.487.468-1.068zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.7 13.7 0 0 1-.312 2.5m2.802-3.5a7 7 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7 7 0 0 0-3.072-2.472c.218.284.418.598.597.933M10.855 4a8 8 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4z"/>
-								</svg>
-              </a>
-            </li>
-						<li class="nav-item">
-              <a class="nav-link txt-white" href="#!">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
-                  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
-                </svg>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link txt-white" href="#!">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
-                  <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
-                </svg>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </section>
-  <div class="bg-white py-2 py-md-5 py-xl-8 border-top border-light-subtle">
-    <div class="container overflow-hidden">
-      <div class="row gy-4 gy-md-0">
-        <div class="col-xs-12 col-md-7 order-1 order-md-0">
-          <div class="copyright text-center text-md-start" style="font-size: 12px;">
-            Copyright &copy; {{ date('Y') }} Cepat Sehat. All Rights Reserved.
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div class="container">
+			<div class="d-flex align-items-center">
+					<a href="index.html">
+							<img src="{{ asset('assets/img/logo-white-cepat-sehat.svg') }}" class="logo" alt="">
+					</a>
+					<img src="{{ asset('assets/img/logo-atensi.png') }}" class="logo-atensi ms-auto" alt="">
+			</div>
+			<div class="row mt-4">
+					<div class="col-md-8 text-white">
+							<h4 class="fs-14">PT Cepat Sehat Indonesia</h4>
+							<p>Jalan Peternakan No. 13, Kel. Tengah, Kec. Kramat jati, Kota Adm. Jakarta Timur, Provinsi DKI
+									Jakarta 13510 <br>
+									sehatcepat.com</p>
+					</div>
+					<div class="col-md-12">
+							<div class="d-flex align-items-center">
+									<a href="https://api.whatsapp.com/send/?phone=6282211189009&text&type=phone_number&app_absent=0"
+											class="phone text-white fs-14 d-flex align-items-center">
+											<i class="mdi mdi-phone me-2 fs-24"></i> 0822 1118 9009
+									</a>
+									<div class="sosmed list-inline ms-auto">
+											<a href="#" class="list-inline-item text-white">
+													<i class="mdi mdi-web fs-24"></i>
+											</a>
+											<a href="https://www.facebook.com/klinikcepatsehat" class="list-inline-item text-white">
+													<i class="mdi mdi-facebook fs-24"></i>
+											</a>
+											<a href="https://www.instagram.com/sehatcepat.mobi/ " class="list-inline-item text-white">
+													<i class="mdi mdi-instagram fs-24"></i>
+											</a>
+									</div>
+							</div>
+					</div>
+			</div>
+	</div>
+	<div class="footer-bottom">
+		Copyright © {{ date('Y') }} Cepat Sehat. All Rights Reserved.
+	</div>
 </footer>
--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,10 +1,19 @@
-<nav class="navbar bg-white">
-	<div class="container-fluid px-4">
-		<a class="navbar-brand" href="{{ route('ozone') }}">
-			<img src="{{ asset('assets/img/logo_cepat_sehat.svg') }}" alt="cepat sehat" style="width: 25vw;">
-		</a>
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+<nav class="nav nav-top fixed-top">
+	<div class="container mx-auto" style="max-width: 640px;">
+			<div class="d-flex align-items-center">
+					<a href="index.html" class="nav-brand">
+							<img src="{{ asset('assets/img/logo_cepat_sehat.svg') }}" alt="">
+					</a>
+					<div class="btn-group ms-auto">
+							<button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown"
+									aria-expanded="false">
+									<i class="mdi mdi-earth me-2"></i> EN
+							</button>
+							<ul class="dropdown-menu dropdown-menu-end">
+									<li><button class="dropdown-item" type="button">English</button></li>
+									<li><button class="dropdown-item" type="button">Indonesia</button></li>
+							</ul>
+					</div>
+			</div>
 	</div>
 </nav>
