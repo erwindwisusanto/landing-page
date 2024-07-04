8965c92965c19f9e168c98d2e8d62bfd2093f616
erwin dwi s <erwin@waki.mobi>
2024-07-04 17:16:46 +0700

    add text wa for floating button

--- resources/views/pages/ercell/index.blade.php
+++ resources/views/pages/ercell/index.blade.php
@@ -265,7 +265,7 @@
 	<div class="btn-float" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Consult with Our Medical Team"
 		data-bs-custom-class="custom-tooltip">
 		<div class="btn-circle whatsapp">
-			<a href="https://api.whatsapp.com/send/?phone=6285212500030&amp;text&amp;type=phone_number&amp;app_absent=0"
+			<a href="https://api.whatsapp.com/send/?phone=6285212500030&text=Hello+Ercellskin.com%2C+I+want+a+consultation+or+buy...&type=phone_number&app_absent=0"
 				class="text-white">
 				<i class="fs-24 fa-brands fa-whatsapp"></i>
 			</a>
